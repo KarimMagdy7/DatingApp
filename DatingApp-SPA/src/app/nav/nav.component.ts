2c5a2e88504e1da7f0fa5af81d656316e377556f
Karim <Karim.magdy@valeo.com>
2020-09-14 12:20:04 +0200

    End of Section 4

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  login(): void{
+    this.authService.login(this.model).subscribe(next => {
+      console.log('logged in successfully!');
+    }, error => {
+      console.log('Failed to login!');
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !! token;
+  }
+
+  // tslint:disable-next-line: typedef
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out!');
+  }
+}
